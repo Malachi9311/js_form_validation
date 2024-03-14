28091f271588bdc8e97e5713259dccfc38d542d1
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-14 22:05:41 +0200

    Full name validation

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ const validateName = () => {
 
     if (name.length == 0){
         nameError.innerHTML = "Name is required";
+        return false;
     }
-    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)) {
-        nameError.innerHTML = "Write full name"
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = "Write full name";
+        return false;
     }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    // nameError.style.color = 'green';
+    return true;
 
 }
