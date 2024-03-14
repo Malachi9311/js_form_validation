cb754525f781bdb4bb52369737d3f250801016ab
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-14 21:44:00 +0200

    Full name validation

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var phoneError = document.getElementById('phone-error');
 var emailError = document.getElementById('email-error');
 var messageError = document.getElementById('message-error');
 var submitError = document.getElementById('submit-error');
+
+const validateName = () => {
+    var name = document.getElementById('contact-name').value;
+
+    if (name.length == 0){
+        nameError.innerHTML = "Name is required";
+    }
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)) {
+        nameError.innerHTML = "Write full name"
+    }
+
+}
